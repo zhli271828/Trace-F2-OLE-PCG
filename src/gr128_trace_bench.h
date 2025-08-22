688cdb49d9696058920c0c5e79f94d127c89ded3
Zhe.Li <lzonline01@gmail.com>
2025-08-22 10:25:18 +0800

    add LOSSY SPDZ2k_64 degree 4 Galois rings codes

--- src/gr128_trace_bench.h
+++ src/gr128_trace_bench.h
@@ -10,6 +10,26 @@ struct FFT_GR128_Trace_A {
     struct GR128 **fft_a_tensor_maps;
 };
 
+struct FFT_GR128_D3_Trace_A {
+    // length m*c*poly_size for \sigma^i(a_j)
+    struct GR128_D3 **fft_a;
+    // length m*m*c*c*poly_size for \sigma^i(a_k)*\sigma^j(a_l)
+    struct GR128_D3 **fft_a_tensor;
+};
+
+struct FFT_GR128_D4_Trace_A {
+    // length c*poly_size for a=\sigma^0(a)
+    struct GR128_D4 **fft_a;
+    // length m*m*c*c*poly_size for \sigma^i(a_k)*\sigma^j(a_l)
+    struct GR128_D4 **fft_a_tensor;
+};
+
+void init_FFT_GR128_d3_Trace_A(const struct Param *param, struct FFT_GR128_D3_Trace_A *fft_gr128_d3_trace_a);
+void free_FFT_GR128_d3_Trace_A(const struct Param *param, struct FFT_GR128_D3_Trace_A *fft_gr128_d3_trace_a);
+void init_FFT_GR128_d4_Trace_A(const struct Param *param, struct FFT_GR128_D4_Trace_A *fft_gr128_d4_trace_a);
+void free_FFT_GR128_d4_Trace_A(const struct Param *param, struct FFT_GR128_D4_Trace_A *fft_gr128_d3_trace_a);
+
+
 void gr128_trace_bench_pcg(size_t n, size_t c, size_t t, struct PCG_Time *pcg_time);
 
 void init_FFT_GR128_Trace_A(const struct Param *param, struct FFT_GR128_Trace_A *fft_gr128_trace_a);
